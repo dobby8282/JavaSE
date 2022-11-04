b6761a74110eed1ece77c1db7383f98ffb24ee4c
jemicom <jemicom@gmail.com>
2022-11-04 20:37:48 +0900

    readme 추가요!

---
+++ src/review/Test.java
@@ -0,0 +1,19 @@
+package review;
+
+public class Test {
+	
+	public void test() {
+		System.out.println("protected 테스트중입니다.");
+	}
+	// 오버로딩
+	public void test(String name) {
+		System.out.println("이름 :" + name);
+	}
+	
+	// 오버로딩
+	public void test(int num) {
+		System.out.println("숫자 :" + num);
+	}
+	
+
+}
---
+++ src/review/test2/Test2.java
@@ -0,0 +1,10 @@
+package review.test2;
+
+public class Test2 {
+	
+	static void test2() {
+		System.out.println("test2 protected 테스트중입니다.");
+	}
+
+
+}
