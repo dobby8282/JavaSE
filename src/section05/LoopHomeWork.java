2f399476fc022113214ec48c73f118a31ec59dae
ITSC <itsc@example.com>
2023-07-24 19:50:55 +0900

    별찍기 숙제

--- src/section05/LoopHomeWork.java
+++ src/section05/LoopHomeWork.java
@@ -1,4 +1,5 @@
 package section05;
+
 /*
 
 *
@@ -25,14 +26,14 @@ package section05;
 ******* 
   
 4번
-
-   *				
-  ***				  
- *****				
-*******		
- *****		 
-  ***		 
-   *		
+			i		j 0~6
+   *   		0		j 
+  ***		1		j   
+ *****		2		j 
+*******		3		j 
+ *****		4 		j > 9 - i 
+  ***		5 		j > 9 - i
+   *		6		j > 9 - i 
  
 5번 구구단
 	 2 x 1 = 2 	3 x 1 = 3	4 x 1 = 4
@@ -40,8 +41,40 @@ package section05;
 
  */
 public class LoopHomeWork {
+	public static void main(String[] args) {
 
+		for(int i = 0; i < 7; i++) {
+			for(int j = 0; j < 7; j++) {
+				if(i < 7/2 +1) {
+					
+					if(j < 3 - i) {
+						System.out.print(" ");
+					} else if (j > i + 3) {
+						System.out.print(" ");
+					} else {
+						System.out.print("*");
+					}
+					
+				} else {
+					
+					if (j < i - 3) {
+						System.out.print(" ");
+					} else if (j > 9 - i) {
+						System.out.print(" ");
+					} else {
+						System.out.print("*");
+					}
+				}
+			}
+			System.out.println(); 	// 개행
+		}
+
+	}
 }
+
+
+
+
 
 
 
