f5aab080d4f739cf85fe391727ec50aec19c685e
ITSC <itsc@example.com>
2023-07-24 20:06:57 +0900

    숙제 완료

--- src/section05/LoopHomeWork.java
+++ src/section05/LoopHomeWork.java
@@ -46,7 +46,6 @@ public class LoopHomeWork {
 		for(int i = 0; i < 7; i++) {
 			for(int j = 0; j < 7; j++) {
 				if(i < 7/2 +1) {
-					
 					if(j < 3 - i) {
 						System.out.print(" ");
 					} else if (j > i + 3) {
@@ -54,9 +53,7 @@ public class LoopHomeWork {
 					} else {
 						System.out.print("*");
 					}
-					
 				} else {
-					
 					if (j < i - 3) {
 						System.out.print(" ");
 					} else if (j > 9 - i) {
