18f4fa6b39a812eee6bbc42acfef93f008e49a94
ITSC <itsc@example.com>
2023-07-24 20:09:23 +0900

    *

--- src/section05/LoopHomeWork.java
+++ src/section05/LoopHomeWork.java
@@ -43,29 +43,43 @@ package section05;
 public class LoopHomeWork {
 	public static void main(String[] args) {
 
-		for(int i = 0; i < 7; i++) {
+//		for(int i = 0; i < 7; i++) {
+//			for(int j = 0; j < 7; j++) {
+//				if(i < 7/2 +1) {
+//					if(j < 3 - i) {
+//						System.out.print(" ");
+//					} else if (j > i + 3) {
+//						System.out.print(" ");
+//					} else {
+//						System.out.print("*");
+//					}
+//				} else {
+//					if (j < i - 3) {
+//						System.out.print(" ");
+//					} else if (j > 9 - i) {
+//						System.out.print(" ");
+//					} else {
+//						System.out.print("*");
+//					}
+//				}
+//			}
+//			System.out.println(); 	// 개행
+//		}
+		
+		System.out.println("   ☆");
+		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < 7; j++) {
-				if(i < 7/2 +1) {
-					if(j < 3 - i) {
-						System.out.print(" ");
-					} else if (j > i + 3) {
-						System.out.print(" ");
-					} else {
-						System.out.print("*");
-					}
+				if(j < 3 - i) {
+					System.out.print(" ");
+				} else if (j > i + 3) {
+					System.out.print(" ");
 				} else {
-					if (j < i - 3) {
-						System.out.print(" ");
-					} else if (j > 9 - i) {
-						System.out.print(" ");
-					} else {
-						System.out.print("*");
-					}
+					System.out.print("*");
 				}
 			}
 			System.out.println(); 	// 개행
 		}
-
+		System.out.println("  | | Merry Christmas ~*");
 	}
 }
 
